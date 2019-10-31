0ce190194782c62d04fc400dbcf181870061992b
zhangyongwang <20225614@qq.com>
2019-11-01 00:13:57 +0800

    请求xml 请求json

--- public/main.js
+++ public/main.js
@@ -1,3 +1,32 @@
+getJSON.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open('GET', '/5.json')
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.response)
+            const object = JSON.parse(request.response)
+            myName.textContent = object.name
+            console.log(object)
+        }
+    }
+    request.send()
+}
+
+
+getXML.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open('GET', '/4.xml')
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            const dom = request.responseXML
+            const text = dom.getElementsByTagName('warning')[0].textContent
+            console.log(text.trim())
+        }
+    }
+    request.send()
+}
+
+
 getHTML.onclick = () => {
     const request = new XMLHttpRequest()
     request.open('GET', '/3.htm')
@@ -11,6 +40,7 @@ getHTML.onclick = () => {
     }
     request.send()
 }
+
 getJS.onclick = () => {
     const request = new XMLHttpRequest()
     request.open('GET', '/2.js')
@@ -24,7 +54,6 @@ getJS.onclick = () => {
     }
     request.send()
 }
-
 
 getCSS.onclick = () => {
     const request = new XMLHttpRequest()
--- server.js
+++ server.js
@@ -46,6 +46,16 @@ var server = http.createServer(function (request, response) {
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
         response.write(fs.readFileSync('public/3.html'))
         response.end()
+    } else if (path === '/4.xml') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/xml;charset=utf-8')
+        response.write(fs.readFileSync('public/4.xml'))
+        response.end()
+    } else if (path === '/5.json') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/json;charset=utf-8')
+        response.write(fs.readFileSync('public/5.json'))
+        response.end()
     }
     else {
         response.statusCode = 404
