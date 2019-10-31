d7a999cad8e0b4dde301015292ba85f1d200508a
zhangyongwang <20225614@qq.com>
2019-10-31 22:26:52 +0800

    onreadystatechange 事件

--- public/main.js
+++ public/main.js
@@ -29,13 +29,16 @@ getJS.onclick = () => {
 getCSS.onclick = () => {
     const request = new XMLHttpRequest()
     request.open('GET', '/style.css');
-    request.onload = () => {
-        const style = document.createElement('style')
-        style.innerHTML = request.response
-        document.head.appendChild(style)
-    }
-    request.onerror = () => {
-        console.log('fail...')
+    request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                const style = document.createElement('style')
+                style.innerHTML = request.response
+                document.head.appendChild(style)
+            } else {
+                alert('加载 CSS 失败')
+            }
+        }
     }
     request.send()
 }
